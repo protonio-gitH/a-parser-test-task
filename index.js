da8c219aafe1c31c6ce54e38a17961233e1a7206
protonio git-H <profiloh@mail.ru>
2024-07-15 21:16:54 +0300

    fixed

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import axios from "axios";
-import cheerio from "cheerio";
 
 async function parse(url){
-    try{
+    try {
         const response = await axios.get(url);
+        const html = response.data;
+        const regex = /<div class="integration-card-copy most-popular">.*?<strong(?: class="bold-text(?:-\d+)?)?">(.*?)<\/strong>.*?<div>(.*?)<\/div>/gs
+        
+        let match;
+        while ((match = regex.exec(html)) !== null) {
+            const strongText = match[1];
+            const emptyDivText = match[2];
+            const str = /(.*?)<strong class="bold-text(-\d+)">(.*?)<\/strong>/.exec(emptyDivText)
+            if (str){
+                console.log(`${strongText}, ${str[1]} ${str[3]}`);
+            }else{
+                console.log(`${strongText.replace(/<br>/,'')}, ${emptyDivText}`)
+            }
+        }
 
-        const $ = cheerio.load(response.data);
 
-        $('.integration-card-copy').each((index, element) => {
-            const h4Text = $(element).find('h4').text();
-            console.log(`${h4Text},${$(element).find('div:not([class])').text()}`);
-        });
-
-    }catch(err){
+    }
+    catch(err){
         console.error(`Ошибка при запросе: ${err.message}`);
     }
-    
 }
 
 
 parse('https://a-parser.com/parsers/');
+
+
