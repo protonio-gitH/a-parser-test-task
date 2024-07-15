d73b81242245fae9709e640ad02a548b8b9c17ca
protonio git-H <profiloh@mail.ru>
2024-07-15 18:37:10 +0300

    fix

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ async function parse(url){
 
         $('.integration-card-copy').each((index, element) => {
             const h4Text = $(element).find('h4').text();
-            const regExp = new RegExp('^SE::');
-            if (regExp.test(h4Text)){
-                console.log(`${h4Text},${$(element).find('div:not([class])').text()}`);
-
-            }
+            console.log(`${h4Text},${$(element).find('div:not([class])').text()}`);
         });
 
     }catch(err){
